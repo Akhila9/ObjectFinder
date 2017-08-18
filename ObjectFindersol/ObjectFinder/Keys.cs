37b1151067afb605e52528b44cc2ebb6fff52e37
Akhila9 <akhilatupili@gmail.com>
2017-08-19 01:13:34 +0530

    Update Keys.cs

--- ObjectFindersol/ObjectFinder/Keys.cs
+++ ObjectFindersol/ObjectFinder/Keys.cs
@@ -7,7 +7,7 @@ namespace ObjectFinder
 {
     public class Keys
     {
-        public static string bingMapsKey = @"Ak0TW4OvykAN8o0ThhpJQtlPl5GG1wN0n29SjuGE1lhm65fcDh6lC42vs6WHaLAZ";
+        public static string bingMapsKey = @"Bing Maps API";
 
     }
 }
